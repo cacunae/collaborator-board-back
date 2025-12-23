1e5d50d3dd9a7c18a0110297116c08e2289bbad1
Cristobal <nakar.zeta@gmail.com>
2025-12-23 01:01:29 -0300

    Se agrega exposisión por ngrok

--- backend/server.js
+++ backend/server.js
@@ -4,24 +4,36 @@ import cors from "cors";
 import { Server } from "socket.io";
 import socketHandlers from "./src/socketHandlers.js";
 
+
 const app = express();
-app.use(cors());
+
+const corsOptions = {
+  origin: "*", 
+  methods: ["GET", "POST"],
+  credentials: false,
+  allowedHeaders: ["Content-Type", "Authorization", "ngrok-skip-browser-warning"]
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send({ status: "Backend running" });
+  res.send({ status: "Backend Open" });
 });
 
 const server = http.createServer(app);
+
 const io = new Server(server, {
-  cors: {
-    origin: "*", 
-  }
+  cors: corsOptions,
+  transports: ["websocket", "polling"]
 });
 
-io.on("connection", (socket) => socketHandlers(io, socket));
+io.on("connection", (socket) => {
+  console.log(" Cliente conectado:", socket.id);
+  socketHandlers(io, socket);
+});
 
 const PORT = 3001;
 server.listen(PORT, () => {
-  console.log(`Backend realtime corriendo en puerto ${PORT}`);
+  console.log(`Servidor corriendo en ${PORT}`);
 });
